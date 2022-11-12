af4ca829229925168e9305289e19c08268f1090f
Rodolfo Sousa <rodolfozsousa@gmail.com>
2022-11-12 02:22:20 -0300

    changed default database url

--- dist/database/db.js
+++
@@ -1,6 +0,0 @@
-import pg from 'pg';
-var Pool = pg.Pool;
-var connection = new Pool({
-    connectionString: 'postgresql://postgres:123456@localhost:5432/tbr'
-});
-export default connection;
--- src/database/db.ts
+++ src/database/db.ts
@@ -3,10 +3,10 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connection = new Pool({
-  connectionString: 'postgresql://postgres:123456@localhost:5432/tbr',
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  connectionString: 'postgres://yqvjddfxloheak:a725f0370d35028010edf5b734d1c0114fee331812b3b011387d76b04410a303@ec2-3-209-39-2.compute-1.amazonaws.com:5432/da0qmfffno84kr',
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default connection;
